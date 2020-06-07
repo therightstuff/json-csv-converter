bdf56cd9eab3a62a73616f1985ced51378653d58
therightstuff <adam@industrialcuriosity.com>
2020-06-07 10:37:53 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # json-csv-converter
+
 JSON / CSV conversion interface
+
+The latest version can be loaded directly from the GitHub repo [here](http://htmlpreview.github.io/?https://github.com/therightstuff/json-csv-converter/blob/master/dist/index.html).
---
+++ src/windowJson2Csv.js
@@ -0,0 +1,19 @@
+﻿"use strict";
+
+/*
+	The node.js modules to be ported to the client must be added
+	to the window.json2csv object.
+
+    "npm build" must be run after updating this module
+
+    Once bundle has been included in html page with following directive
+        <script src="json-2-csv.js"></script>
+    javascript calls can be made to converter.* eg
+        let json2csvCallback = function (err, csv) {
+            if (err) throw err;
+            console.log(csv);
+        };
+        converter.json2csv(documents, json2csvCallback);
+*/
+
+window.converter = require('json-2-csv');
